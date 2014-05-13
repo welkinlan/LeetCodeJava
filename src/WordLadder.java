20045bf8b9d2970aeebb4379bc90be389b62fd3f
welkinlan <welkinlan@gmail.com>
2014-05-13 14:58:47 -0500

    wordladder

--- src/WordLadder.java
+++ src/WordLadder.java
@@ -13,7 +13,42 @@ public class WordLadder {
     static HashMap <String, Integer> stringVisited = new HashMap <String, Integer>();
     static int best = 0;
 
-    public static int ladderLength(String start, String end, HashSet<String> dict) {
+    public int ladderLength(String start, String end, HashSet<String> dict) {
+        if (dict.size() == 0)
+            return 0;
+
+        LinkedList<String> wordQueue = new LinkedList<String>();
+        LinkedList<Integer> distanceQueue = new LinkedList<Integer>();
+
+        wordQueue.add(start);
+        distanceQueue.add(1);
+
+        while(!wordQueue.isEmpty()){
+            String currWord = wordQueue.pop();
+            Integer currDistance = distanceQueue.pop();
+
+            if(currWord.equals(end)){
+                return currDistance;
+            }
+
+            for(int i=0; i<currWord.length(); i++){
+                char[] currCharArr = currWord.toCharArray();
+                for(char c='a'; c<='z'; c++){
+                    currCharArr[i] = c;
+
+                    String newWord = new String(currCharArr);
+                    if(dict.contains(newWord)){
+                        wordQueue.add(newWord);
+                        distanceQueue.add(currDistance+1);
+                        dict.remove(newWord);
+                    }
+                }
+            }
+        }
+        return 0;
+    }
+
+    public static int myladderLength(String start, String end, HashSet<String> dict) {
         //add start and end string to dict
         dict.add(start);
         dict.add(end);
