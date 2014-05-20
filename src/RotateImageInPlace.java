35cf4e3f06e637f06e6573b1cef2c7833d9d8698
welkinlan <welkinlan@gmail.com>
2014-05-20 17:40:09 -0500

    RotateImageInPlace

--- src/RotateImageInPlace.java
+++ src/RotateImageInPlace.java
@@ -2,6 +2,22 @@
  * Created by tianlan on 5/15/2014.
  */
 public class RotateImageInPlace {
+    public static void rotate(int[][] matrix) {
+        int n = matrix.length;
+        for (int round = 0; round < n/2; round++){
+            int start = round;
+            int end = n-round-1; //the last element for each row/col does not need to be swapped
+            for (int i = start; i < end; i++){
+                int temp = matrix[i][end];
+                //top row to rightmost col
+                matrix[i][end];
+            }
+        }
 
+    }
+
+    public static void main(String[] args){
+
+    }
 
 }
