c001d61e4e8bff34abc3621cce56823effc26ab8
welkinlan <welkinlan@gmail.com>
2014-05-21 11:09:12 -0500

    RotateImageInPlace

--- src/RotateImageInPlace.java
+++ src/RotateImageInPlace.java
@@ -5,18 +5,41 @@ public class RotateImageInPlace {
     public static void rotate(int[][] matrix) {
         int n = matrix.length;
         for (int round = 0; round < n/2; round++){
+            //the start and end of the new round (circle)
             int start = round;
-            int end = n-round-1; //the last element for each row/col does not need to be swapped
-            for (int i = start; i < end; i++){
-                int temp = matrix[i][end];
-                //top row to rightmost col
-                matrix[i][end];
+            int end = n-round-1;
+            //the last element for each row/col does not need to be swapped, so "<"
+            for (int i = 0; i < end-start; i++){
+                //store top row value as temp
+                int temp = matrix[start][start+i];
+                //leftmost col -> top row
+                matrix[start][start+i] = matrix[end-i][start];
+                //bottom row -> leftmost col
+                matrix[end-i][start] = matrix[end][end-i];
+                //rightmost col -> bottom row
+                matrix[end][end-i] =  matrix[start+i][end];
+                //top row -> rightmost col
+                matrix[start+i][end] = temp;
             }
         }
-
     }
 
     public static void main(String[] args){
+        //int a[][] = new int[][]{{1,2,3},{4,5,6},{7,8,9}};
+        int a[][] = new int[][]{{1,2,3,4},{5,6,7,8},{9,10,11,12},{13,14,15,16}};
+        for (int i=0; i<a.length; i++){
+            for(int j=0; j<a.length; j++){
+                System.out.print(a[i][j]+",");
+            }
+            System.out.print("\n");
+        }
+        rotate(a);
+        for (int i=0; i<a.length; i++){
+            for(int j=0; j<a.length; j++){
+                System.out.print(a[i][j]+",");
+            }
+            System.out.print("\n");
+        }
 
     }
 
