78e5e9a98791703bdfd550dd767c64bbb148cd08
welkinlan <welkinlan@gmail.com>
2014-05-22 17:14:13 -0500

    RegularExpressionMatching

--- src/RegularExpressionMatching.java
+++ src/RegularExpressionMatching.java
@@ -3,12 +3,18 @@
  */
 public class RegularExpressionMatching {
 
-    public boolean isMatch(String s, String p) {
+    public static boolean isMatch(String s, String p) {
         return false;
     }
 
 
     public static void main(String[] args){
-
+        System.out.print(isMatch("aa","a"));
+        System.out.print(isMatch("aa","aa"));
+        System.out.print(isMatch("aaa","aa"));
+        System.out.print(isMatch("aa", "a*"));
+        System.out.print(isMatch("aa", ".*"));
+        System.out.print(isMatch("ab", ".*"));
+        System.out.print(isMatch("aab", "c*a*b"));
     }
 }
