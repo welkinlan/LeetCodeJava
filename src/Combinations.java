520144239eaf1086beff5ae9881810d011421541
Tian Lan <welkinlan@gmail.com>
2014-05-24 17:01:47 -0500

    Combinations

--- src/Combinations.java
+++ src/Combinations.java
@@ -8,37 +8,38 @@ public class Combinations {
 
     static int N;
     static int K;
+    static ArrayList<ArrayList<Integer>> combination;
 
 
     public static ArrayList<ArrayList<Integer>> combine(int n, int k) {
         if (n==0 || k==0 || k>n) return null;
         N = n;
         K = k;
-        ArrayList<ArrayList<Integer>> combination = new ArrayList<ArrayList<Integer>>();
-        for (int first=1; first<n-k ; first++){
-            for (int second=first+1; second<=n; second++){
-                ArrayList<Integer> item = new ArrayList<Integer>();
-                item.add(first);item.add(second);
-                combination.add(item);
-            }
+        combination = new ArrayList<ArrayList<Integer>>();
+        for (int now=1; now <= N-K+1; now++){
+            //System.out.println("-------");
+            minusKCombination(new ArrayList<Integer>(), now, K);
         }
-
+        //System.out.println("-----result------");
         return combination;
     }
 
-    public static void recursiveOutput(int i, String s){
-        if (i == chars.length){
-            result.add(s);
+    public static void minusKCombination(ArrayList<Integer> tempAL, int now, int k){
+        tempAL.add(now);
+        //System.out.println(tempAL+",k="+k);
+        if (k==1) { //no other options available
+            combination.add(tempAL);
+            //System.out.println(tempAL);
         }
-        else{
-            List<Character> options = digitChar.get(chars[i]);
-            for (char c : options){
-                recursiveOutput(i+1, s+c);
+        else {
+            for (int next=now+1; next<=N-k+2; next++){
+                ArrayList<Integer> newAL = new ArrayList<Integer>(tempAL); //create a deep copy of the existing arraylist
+                minusKCombination(newAL, next, k-1);
             }
         }
     }
 
     public static void main(String[] args){
-
+        System.out.print(combine(5,2));
     }
 }
