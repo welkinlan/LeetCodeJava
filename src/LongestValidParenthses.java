63f79c3fcfa0bba9a608c1ee61495fa1d686720f
Tian Lan <welkinlan@gmail.com>
2014-05-27 00:48:23 -0500

    Update

---
+++ src/LongestValidParenthses.java
@@ -0,0 +1,41 @@
+import java.util.LinkedList;
+
+/**
+ * Created by tianlan on 5/26/14.
+ */
+public class LongestValidParenthses {
+
+    public static int longestValidParentheses(String s) {
+        if(s==null || s.length()==0)
+            return 0;
+        LinkedList<Integer> stack = new LinkedList<Integer>();
+        int start = 0;
+        int max = 0;
+        for(int i=0;i<s.length();i++)
+        {
+            if(s.charAt(i)=='(')
+            {
+                stack.push(i);
+            }
+            else
+            {
+                if(stack.isEmpty())
+                {
+                    start = i+1;
+                }
+                else
+                {
+                    stack.pop();
+                    max = stack.isEmpty()?Math.max(max,i-start+1):Math.max(max,i-stack.peek());
+                }
+            }
+        }
+        return max;
+
+    }
+
+    public static void main(String[] args){
+        System.out.print(longestValidParentheses("()())()"));
+    }
+
+}
