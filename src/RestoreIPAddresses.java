17f15696f4459bd8b57b802b231096c675440f74
welkinlan <welkinlan@gmail.com>
2014-07-16 17:31:45 -0500

    Update

---
+++ src/RestoreIPAddresses.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+/**
+ * Created by tianlan on 7/16/2014.
+ */
+public class RestoreIPAddresses {
+    public List<String> restoreIpAddresses(String s) {
+        if (s==null || s.length()==0 || s.length()>12) return null;
+        List<String> res = new ArrayList<String>();
+
+        return res;
+    }
+}
